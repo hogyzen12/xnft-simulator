af882aec4075687a68d5d105601428db9c04b32c
hogyzen12 <billypapas12@gmail.com>
2023-04-09 18:36:27 +0200

    adding metame randomiser and site integration

--- src/screens/ExamplesScreen.tsx
+++ src/screens/ExamplesScreen.tsx
@@ -3,7 +3,7 @@ import * as Linking from "expo-linking";
 import { atom, useRecoilState } from "recoil";
 
 import { Section } from "../components/Section";
-import { Screen } from "../components/Screen";
+import { Screen } from "../components/Screen";2
 
 const testAtom = atom<"native" | "bright">({
   key: "testAtom",
@@ -19,35 +19,45 @@ export function ExamplesScreens() {
 
   return (
     <Screen>
-      <Section title="Recoil">
+
+      <Section title="Welcome to Metame">
         <Button
-          title={`The Future is ${future}`}
-          color={
-            future === "bright" ? "rgb(228, 208, 10)" : "rgb(33, 150, 243)"
-          }
-          onPress={() => setFuture(future === "bright" ? "native" : "bright")}
+          title={`Change Image`}
+          color="rgb(33, 150, 243)"
+          onPress={() => setFuture(String(Math.floor(Math.random() * 200) + 1) + ".png")}
         />
-      </Section>
-      <Section title="Local Image Import">
-        <Image
-          source={require("../../assets/icon.png")}
-          style={{ width: 50, height: 50 }}
+
+        <Image 
+          source={{uri: 'https://shdw-drive.genesysgo.net/EcrJqYViqdV3Sc8r35oU7igJH6soaVewVK6cUorvyMsA/' + future}}
+          style={{width: 300, height: 300}}
         />
-        <LearnMoreLink url="https://reactnative.dev/docs/images#static-image-resources" />
+
+        <Button
+          onPress={() => Linking.openURL("https://metame.vercel.app/")}
+          title="Open Metame"
+        />
+
       </Section>
+    </Screen>
+  );
+}
+
+      {/*
       <Section title="Custom Font">
         <Text style={{ fontFamily: "Inter_900Black" }}>
           Inter 900 Black Font
         </Text>
         <LearnMoreLink url="https://docs.expo.dev/guides/using-custom-fonts/#using-a-google-font" />
       </Section>
+      */}
+
+      {/*
       <Section title="Opening a URL">
         <Button
-          onPress={() => Linking.openURL("https://xnft.gg")}
-          title="Open xNFT.gg"
+          onPress={() => Linking.openURL("https://metame.vercel.app/")}
+          title="Open Metame"
         />
         <LearnMoreLink url="https://docs.expo.dev/versions/latest/sdk/linking/#linkingopenurlurl" />
       </Section>
-    </Screen>
-  );
-}
+      */}
+
